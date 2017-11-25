8cde246d48ecb18bb27d5e95ca66305629ea23bc
antonenko.p <antonenko.pvl@gmail.com>
2017-11-25 17:57:52 +0300

    Add styles for Source page

--- src/models/source-model.js
+++ src/models/source-model.js
@@ -6,15 +6,22 @@ export class Source extends Template {
         super();
         this.id = source.id;
         this.name = source.name;
+        this.url = source.url;
     }
 
     getHtml() {
         return `
         <div class="source">
-            <a href="#${HEADLINES}?sources=${this.id}">
-                ${this.name}
+            <a 
+                href="#${HEADLINES}?sources=${this.id}" 
+                style="background-image:url(${this.getLink()})" 
+                title=" ${this.name}">
             </a>
         </div>
         `;
+    }
+
+    getLink() {
+        return `https://icons.better-idea.org/icon?url=${this.url}&size=70..120..200`;
     }
 }
